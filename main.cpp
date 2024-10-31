b88a40ec8968acae0b37762fd1c07ec44d18e512
ikhamula <ilyahamula95@gmail.com>
2024-10-31 22:11:30 +0200

    added main.cpp and cmakelists

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <glm/glm.hpp>
+#include <glfw/glfw3.h>
+#include <iostream>
+
+int main()
+{
+    // Приклад використання GLM
+    glm::vec3 vector(1.0f, 0.0f, 0.0f);
+    std::cout << "Vector: (" << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
+
+    // Ініціалізація GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Не вдалося ініціалізувати GLFW!\n";
+        return -1;
+    }
+    glfwTerminate();
+    return 0;
+}
